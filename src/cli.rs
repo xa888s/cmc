612e14f3d5082e90f1ecc13f7ccb8cb0a79ca15c
abyss <derribado@tutanota.com>
2021-04-28 15:44:31 -0700

    Separated CLI stuff out a bit to help with legibility

---
+++ src/cli.rs
@@ -0,0 +1,35 @@
+use argh::FromArgs;
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(description = "simple crackmes.one client")]
+pub struct App {
+    #[argh(subcommand)]
+    pub nested: Commands,
+}
+
+#[non_exhaustive]
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+pub enum Commands {
+    Get(SubGet),
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(description = "used to get crackmes", subcommand, name = "get")]
+pub struct SubGet {
+    #[argh(
+        description = "the ID of the crackme",
+        from_str_fn(id_parser),
+        positional
+    )]
+    pub id: String,
+}
+
+fn id_parser(v: &str) -> Result<String, String> {
+    let v = v.to_string();
+    if v.len() != 24 {
+        Err(v)
+    } else {
+        Ok(v)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,11 @@
 use anyhow::Result;
-use argh::FromArgs;
 use reqwest::Client;
+
+mod cli;
 mod get;
 mod search;
 
-#[derive(FromArgs, PartialEq, Debug)]
-#[argh(description = "simple crackmes.one client")]
-struct App {
-    #[argh(subcommand)]
-    nested: Commands,
-}
-
-#[non_exhaustive]
-#[derive(FromArgs, PartialEq, Debug)]
-#[argh(subcommand)]
-enum Commands {
-    Get(SubGet),
-}
-
-#[derive(FromArgs, PartialEq, Debug)]
-#[argh(description = "used to get crackmes", subcommand, name = "get")]
-struct SubGet {
-    #[argh(
-        description = "the ID of the crackme",
-        from_str_fn(id_parser),
-        positional
-    )]
-    id: String,
-}
-
-fn id_parser(v: &str) -> Result<String, String> {
-    let v = v.to_string();
-    if v.len() != 24 {
-        Err(v)
-    } else {
-        Ok(v)
-    }
-}
+use cli::*;
 
 #[tokio::main]
 async fn main() -> Result<()> {
